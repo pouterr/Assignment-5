214fd71fac0cc147031450b408d48e003ade78a0
pouterr <poutergutorov@gmail.com>
2023-05-16 14:13:07 +0600

    created BST.java class

---
+++ src/BST.java
@@ -0,0 +1,27 @@
+public class BST<K extends Comparable<K>, V>{
+    private Node root;
+    private class Node{
+        private K key;
+        private V val;
+        private Node left,right;
+        public Node(K key,V val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+    public void put(K key,V value){
+
+    }
+
+    public V get(K key){
+
+    }
+
+    public void delete(K key){
+
+    }
+
+    public Iterable<K> iterator(){
+
+    }
+}
